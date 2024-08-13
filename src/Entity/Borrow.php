bd7698f26510e7f6ef446df8cf35f264aa5e3789
mateuszlasek <mateusz.lasek17@wp.pl>
2024-08-13 09:01:25 +0200

    create borrow entity

---
+++ src/Entity/Borrow.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BorrowRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BorrowRepository::class)]
+class Borrow
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Book::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Book $book = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $borrowedAt = null;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTimeInterface $returnedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): self
+    {
+        $this->book = $book;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getBorrowedAt(): ?\DateTimeInterface
+    {
+        return $this->borrowedAt;
+    }
+
+    public function setBorrowedAt(\DateTimeInterface $borrowedAt): self
+    {
+        $this->borrowedAt = $borrowedAt;
+        return $this;
+    }
+
+    public function getReturnedAt(): ?\DateTimeInterface
+    {
+        return $this->returnedAt;
+    }
+
+    public function setReturnedAt(?\DateTimeInterface $returnedAt): self
+    {
+        $this->returnedAt = $returnedAt;
+        return $this;
+    }
+}
